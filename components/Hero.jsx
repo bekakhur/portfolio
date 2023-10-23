98a8af754ac0f5f3d6ac4bcb0ec505232d849979
Mac <mac@MacBook-Pro-Mac.local>
2023-10-23 04:29:34 +0300

    second commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="gap-20 flex min-h-[800px] lg:min-h-[600px] flex-col lg:flex-row-reverse h-screen items-center w-full pb-10 justify-center">
       <div className="h-[300px] min-h-[300px] w-[300px] md:w-[22rem] md:h-[22rem] bg-red-400 rounded-full"></div>
       <div className="flex flex-col px-8 lg:w-[47%] max-w-[600px] items-center text-center gap-8">
-        <h1 className="text-4xl lg:text-6xl font-bold">John Jones</h1>
+        <h1 className="text-4xl lg:text-6xl font-bold">John Jones Jr</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit
           obcaecati adipisci illo ullam sed, iusto vel delectus, explicabo
